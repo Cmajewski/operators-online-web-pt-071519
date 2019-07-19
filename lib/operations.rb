bdb1718df2fdedb88cbeeeeaad0f221631ab6bfb
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-19 20:17:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,10 @@
 def unsafe?(speed)
-if speed<40 ||speed>60
+ if speed<40||speed>60
+ else false
 end
 
 
 
 def not_safe?(speed)
-	speed<40||speed>60? true:false
+	speed<40||speed>60 ? true:false
 end
